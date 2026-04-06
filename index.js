2f5f50d260aca71dbab082bb449666e2cfb93795
suhana <suhanap064@gmail.com>
2026-04-06 11:31:11 +0530

    update

--- index.js
+++ index.js
@@ -39,3 +39,13 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('🚀 Server is running successfully on Render and connected to MongoDB Atlas!');
 });
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
+
+
+//Export app (for testing or vecel if reused)
+module.exports=app;
